ba00ccaf4ade797d0e1c7a247e3cbcf54c954bd9
DavidAkerlind <david.akerlind@icloud.com>
2026-02-24 09:39:16 +0100

    lecture 4 start

---
+++ lecture_3.s
@@ -0,0 +1,27 @@
+# Division 
+
+# Dividend = nominator 1001010
+
+# Divisor = denominator 1000
+
+# fit the divisor in the divisor one bit in a time from the left 
+a
+# Qoutient:1001
+# 1000)	1001010
+#________________
+#      -1000
+#	   10
+#	   101
+#	   1010
+#	  -1000
+#  reminder  10
+
+
+
+# use the hight en lo
+
+#1 00011111 11011111100000000000000
+
+
+
+
---
+++ lecture_4.s
@@ -0,0 +1,5 @@
+# Assembly programing
+
+
+
+
